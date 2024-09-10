e7632c58cee129b6f8a3fc881254db2508d14d12
AngelLuna6969 <utp0149900@alumno.utpuebla.edu.mx>
2024-09-10 08:50:20 -0600

    implementacion de useFetch

--- components/PokemonInfo.tsx
+++ components/PokemonInfo.tsx
@@ -2,28 +2,30 @@ import { View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Avatar, Button, Text } from 'react-native-paper'
 import { useCounter } from '@/hooks/useCounter';
+import { useFetch } from '@/hooks/useFetch';
 
 export const PokemonInfo = () => {
 
-    const [name, setName] = useState("");
-    const [ide, setIde] = useState("");
-    const [front, setFont] = useState("");
-
     const { counter, increment, decrement } = useCounter();
+    const {data, isLoading, hasError, error} = useFetch("https://pokeapi.co/api/v2/pokemon/" + counter);
 
-    const getPkemonInfo = async (id = 1) => {
-        const url = "https://pokeapi.co/api/v2/pokemon/" + id;
-        const request = await fetch(url);
-        const reponse = await request.json();
-        setName(reponse.name);
-        setIde(reponse.id);
-        setFont(reponse.sprites.front_default)
-        console.log(reponse);
-    }
+    // const [name, setName] = useState("");
+    // const [ide, setIde] = useState("");
+    // const [front, setFont] = useState("");
 
-    useEffect(() => {
-        getPkemonInfo(counter);
-    }, []);
+    // const getPkemonInfo = async (id = 1) => {
+    //     const url = "https://pokeapi.co/api/v2/pokemon/" + id;
+    //     const request = await fetch(url);
+    //     const reponse = await request.json();
+    //     setName(reponse.name);
+    //     setIde(reponse.id);
+    //     setFont(reponse.sprites.front_default)
+    //     console.log(reponse);
+    // }
+
+    // useEffect(() => {
+    //     getPkemonInfo(counter);
+    // }, [counter]);
 
     return (
         <View style={{ flex: 1, justifyContent:"center", alignItems:"center"}}>
